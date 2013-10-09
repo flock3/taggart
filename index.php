8264b443744c25e85e677d45f7a91f19da7891ac
Thomas Gray <tom@osg.me>
2013-10-09 19:30:55 +0100

    Adding index changes to include database

--- index.php
+++ index.php
@@ -7,8 +7,25 @@ $app->config('templates.path', realpath(__DIR__ . '/views'));
 
 $config = require(realpath(__DIR__)  . '/config.php');
 
+/**
+ * Autoloader for the app/ directory
+ */
 require(realpath(__DIR__) . '/app/autoloader.php');
+
+/**
+ * Loader of the ServiceLoader (app/Service)
+ */
 require(realpath(__DIR__) . '/app/service.php');
+
+/**
+ * Loader of the database handler
+ */
+require(realpath(__DIR__) . '/app/database.php');
+
+/**
+ * Loader of the routes (the actual application really)
+ */
 require(realpath(__DIR__) . '/app/routes.php');
+
 
 $app->run();
